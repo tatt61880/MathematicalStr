2ba7a2fe021bfc8a6f0ed4a7f5bef0ca421ce0a5
tatt61880 <tatt61880@gmail.com>
2022-05-26 22:50:00 +0900

    Version: 2022.05.26-c

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 (function() {
   'use strict';
-  const version = 'Version: 2022.05.26-b';
+  const version = 'Version: 2022.05.26-c';
 
   window.addEventListener('load', init, false);
 
   let elemText;
   const elemResults = [];
-  const kNum = 4;
+  const codes = [
+    56320,
+    56424,
+    56528,
+    56684,
+    56736,
+    56788,
+    56840,
+    56892,
+  ];
+  let typeNum = codes.length;
 
   function init() {
     document.getElementById('versionInfo').innerText = version;
     
     elemText = document.getElementById('inputText');
     elemText.addEventListener('input', updateResult, false);
-    for (let i = 1; i <= kNum; ++i) {
+    for (let i = 0; i < typeNum; ++i) {
       elemResults[i] = document.getElementById(`result${i}`);
     }
     updateResult();
@@ -22,30 +32,28 @@
   function updateResult() {
     const text = elemText.value;
     let resultTexts = [];
-    for (let i = 1; i <= kNum; ++i) {
+    for (let i = 0; i < typeNum; ++i) {
       resultTexts[i] = '';
     }
 
     for (const c of text) {
       const code = c.charCodeAt(0);
       if (0x41 <= code && code <= 0x5a) {
-        resultTexts[1] += String.fromCharCode(55349) + String.fromCharCode(56320 + code - 0x41);
-        resultTexts[2] += String.fromCharCode(55349) + String.fromCharCode(56424 + code - 0x41);
-        resultTexts[3] += String.fromCharCode(55349) + String.fromCharCode(56528 + code - 0x41);
-        resultTexts[4] += String.fromCharCode(55349) + String.fromCharCode(56684 + code - 0x41);
+        for (let i = 0; i < typeNum; ++i) {
+          resultTexts[i] += String.fromCharCode(55349) + String.fromCharCode(codes[i] + code - 0x41);
+        }
       } else if (0x61 <= code && code <= 0x7a) {
-        resultTexts[1] += String.fromCharCode(55349) + String.fromCharCode(56346 + code - 0x61);
-        resultTexts[2] += String.fromCharCode(55349) + String.fromCharCode(56450 + code - 0x61);
-        resultTexts[3] += String.fromCharCode(55349) + String.fromCharCode(56554 + code - 0x61);
-        resultTexts[4] += String.fromCharCode(55349) + String.fromCharCode(56710 + code - 0x61);
+        for (let i = 0; i < typeNum; ++i) {
+          resultTexts[i] += String.fromCharCode(55349) + String.fromCharCode(codes[i] + 26 + code - 0x61);
+        }
       } else {
-        for (let i = 1; i <= kNum; ++i) {
+        for (let i = 0; i < typeNum; ++i) {
           resultTexts[i] += c;
         }
       }
     }
 
-    for (let i = 1; i <= kNum; ++i) {
+    for (let i = 0; i < typeNum; ++i) {
       elemResults[i].innerText = resultTexts[i] == '' ? '　' : resultTexts[i];
     }
   }
